eb100a72d6e6f5dd4c18240f06f9cb1775cc5f0c
Guilherme R.Silva <guilhermers2014@outlook.com>
2024-05-27 20:56:51 -0300

    Aula 02 - Componentização

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,14 @@
+//Maneira antiga de criar Componetes
+import React from "react";
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button>
+                Botao
+            </button>
+        )
+    }
+}
+
+export default Button;
